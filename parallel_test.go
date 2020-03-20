880eac6a203ac646d756dd3cec7ffce331dadf3b
Michael Whatcott <mdwhatcott@gmail.com>
2020-03-20 14:30:26 -0600

    Wrote example test suite.

--- parallel_test.go
+++ parallel_test.go
@@ -1,1 +1,42 @@
 package parallel
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test1(t *testing.T) { runSuite(t) }
+func Test2(t *testing.T) { runSuite(t) }
+
+func runSuite(t *testing.T) {
+	t.Parallel() // run top-level tests in parallel
+
+	for _, test := range testCases {
+		t.Run(test.input, test.execute)
+	}
+}
+
+var testCases = []TestCase{
+	{
+		input:    "this passes",
+		expected: "THIS PASSES",
+	},
+	{
+		input:    "this will fail",
+		expected: "because this is wrong",
+	},
+}
+
+type TestCase struct {
+	input    string
+	expected string
+}
+
+func (test TestCase) execute(t *testing.T) {
+	t.Parallel() // run each sub-test in parallel
+
+	actual := strings.ToUpper(test.input)
+	if actual != test.expected {
+		t.Errorf("\ngot:  [%s]\nwant: [%s]", actual, test.expected)
+	}
+}
